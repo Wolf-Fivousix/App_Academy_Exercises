3bc021e2e89c457909203fb4bfb22f6b59c7d753
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-08-23 13:08:28 -0700

    Add a line of possible conflict

--- branching.rb
+++ branching.rb
@@ -1,2 +1,4 @@
 #This file is just a test file for branc-merging functionality.
 # I have to branch out incoming features. This is an example.
+
+# This is an extra comment after the Merge Conflict was fixed! What happens?
