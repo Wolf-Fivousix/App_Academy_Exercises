726b80e3dd8502e1f3c6a445ac0f039261a11de0
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-08-26 16:06:28 -0700

    Add W2D1 final exercise with Tina

---
+++ Pair Programming Exercises/W2D1/Tina_&_Diego_bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,47 @@
+class Bootcamp
+    attr_reader :name, :slogan, :teachers, :students
+
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new {|hash, key| hash[key] = [] }
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student)
+        # Ternary Operator     ? : 
+        #condition ? TRUE Do This : FALSE Do This
+        return false if @students.length >= @student_capacity
+        @students << student
+        true
+    end
+
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        @students.count / @teachers.count
+    end
+
+    def add_grade(student, grade)
+        return false if !@students.include?(student)
+        @grades[student] << grade
+        true
+    end
+
+    def num_grades(student)
+        @grades[student].count
+    end
+
+    def average_grade(student)
+        return nil if @grades[student].empty? # || !@students.include?(student)
+        @grades[student].sum / num_grades(student)
+    end
+end
