5d2e0d98f8ed3f6605bb73770f5fd131fe85a2aa
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-11-17 23:04:21 -0800

    Add Howl frontpage

---
+++ MERNTwitter/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Howl from "./components/howl";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Howler</h1>
+      <p>Welcome to howler</p>
+      <Howl />
+    </div>
+  );
+}
+
+export default App;
